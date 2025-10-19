c346de4f9b54def8c2e32e0b480b63c3256e57ce
vishnu teli <vishnusindhal5040@gmail.com>
2025-10-19 08:37:48 +0530

    update the port for render

--- backend/server.js
+++ backend/server.js
@@ -48,6 +48,7 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
