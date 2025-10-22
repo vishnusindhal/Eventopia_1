8a8cc6082f69aa5875c0171edeb3494079fe69a5
vishnu teli <vishnusindhal5040@gmail.com>
2025-10-22 07:33:05 +0530

    change in package.json

--- backend/server.js
+++ backend/server.js
@@ -47,8 +47,6 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
-
-
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
