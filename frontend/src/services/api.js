4e171d3ea4621d6fe421714970c0401350238252
vishnu teli <vishnusindhal5040@gmail.com>
2025-11-26 18:47:02 +0530

    update the re-direct link

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -22,7 +22,7 @@ api.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response?.status === 401) {
-      window.location.href = "/login";
+      window.location.href = "/";
     }
     return Promise.reject(error);
   }
