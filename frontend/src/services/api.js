99c9446f0ed8eeb378e67281d8cbae624c616926
vishnu teli <vishnusindhal5040@gmail.com>
2025-11-26 19:23:32 +0530

    change the base url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://eventopia-1-5.onrender.com",
+  baseURL: "https://eventopia-1-5.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
