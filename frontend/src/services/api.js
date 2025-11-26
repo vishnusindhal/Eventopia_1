9e70243cf91968015fc5e3f8654db94aae64d320
vishnu teli <vishnusindhal5040@gmail.com>
2025-11-26 19:27:29 +0530

    change the url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,12 +2,13 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://eventopia-1-5.onrender.com",
+  
   headers: {
     "Content-Type": "application/json",
   },
   withCredentials: true,
 });
-
+console.log('API baseURL =', baseURL);
 // Request interceptor - cookies are automatically sent with requests
 api.interceptors.request.use(
   (config) => {
