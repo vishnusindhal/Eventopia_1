6f357bd01e5ff8ef56266a0bbc296d44b9303ebd
vishnu teli <vishnusindhal5040@gmail.com>
2025-11-26 19:06:47 +0530

    re-direct lonk change

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -22,7 +22,7 @@ api.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response?.status === 401) {
-      window.location.href = "/";
+      window.location.href = "/login";
     }
     return Promise.reject(error);
   }
