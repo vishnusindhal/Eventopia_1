1797cbd4033665e5812ef89099587094c98709cd
vishnu teli <vishnusindhal5040@gmail.com>
2025-12-14 11:22:06 +0530

    generalise base url in env

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://eventopia-1-5.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL,
   
   headers: {
     "Content-Type": "application/json",
